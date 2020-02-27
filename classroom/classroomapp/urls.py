55c19aed04447621d0c04fd08dcd3fa7030c5fef
Sameer Barha <sameer.barha12@gmail.com>
2020-02-27 22:01:08 +0530

    fixed urls

--- classroom/classroomapp/urls.py
+++ classroom/classroomapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name= 'classroomapp'
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path("class_preview", views.class_preview, name="class_preview"),
 ]
